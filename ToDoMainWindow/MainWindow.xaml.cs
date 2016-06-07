39b26a87e255ab11183011fde78c79af609dcd6d
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-06-07 20:41:41 +0700

    Add tasks

--- ToDoMainWindow/MainWindow.xaml.cs
+++ ToDoMainWindow/MainWindow.xaml.cs
@@ -10,3 +10,10 @@
         #endregion
     }
 }
+
+
+// TODO: Use MahApps
+// TODO: Use config (data files, marked colors, v.v...)
+// TODO: Research how to use file on cloud
+// TODO: Notify
+// TODO: Combine MahApps with Extended WPF Controls
