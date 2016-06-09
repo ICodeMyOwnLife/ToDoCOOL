c31140ebe6d3fc91be9ce9ee818a728d07a4fa4c
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-06-09 17:32:41 +0700

    Update UI

--- TodoViewModels/Design/TodoDesignViewModel.cs
+++ TodoViewModels/Design/TodoDesignViewModel.cs
@@ -8,48 +8,52 @@ namespace TodoViewModels.Design
 {
     public class TodoDesignViewModel: TodoViewModel
     {
-        #region  Properties & Indexers
-        public override ObservableCollection<Todo> Todos { get; protected set; } =
-            new ObservableCollection<Todo>(new List<Todo>
-            {
-                new Todo
+        #region  Constructors & Destructor
+        public TodoDesignViewModel()
+        {
+            // ReSharper disable once VirtualMemberCallInContructor
+            Todos =
+                new ObservableCollection<Todo>(new List<Todo>
                 {
-                    Content = "Do job 1",
-                    Deadline = DateTime.Now,
-                    IsDone = false
-                },
-                new Todo
-                {
-                    Content = "Do job 2",
-                    Deadline = DateTime.Now.AddHours(1.5),
-                    IsDone = true
-                },
-                new Todo
-                {
-                    Content = "Do job 3",
-                    Deadline = DateTime.Now.AddHours(3),
-                    IsDone = false
-                }
-                ,
-                new Todo
-                {
-                    Content = "Do job 4",
-                    Deadline = DateTime.Now.AddDays(1.5),
-                    IsDone = false
-                },
-                new Todo
-                {
-                    Content = "Do job 5",
-                    Deadline = DateTime.Now.AddDays(3),
-                    IsDone = true
-                },
-                new Todo
-                {
-                    Content = "Do job 6",
-                    Deadline = DateTime.Now.AddMonths(1),
-                    IsDone = false
-                }
-            });
+                    new Todo
+                    {
+                        Content = "Do job 1",
+                        Deadline = DateTime.Now,
+                        IsDone = false
+                    },
+                    new Todo
+                    {
+                        Content = "Do job 2",
+                        Deadline = DateTime.Now.AddHours(1.5),
+                        IsDone = true
+                    },
+                    new Todo
+                    {
+                        Content = "Do job 3",
+                        Deadline = DateTime.Now.AddHours(3),
+                        IsDone = false
+                    }
+                    ,
+                    new Todo
+                    {
+                        Content = "Do job 4",
+                        Deadline = DateTime.Now.AddDays(1.5),
+                        IsDone = false
+                    },
+                    new Todo
+                    {
+                        Content = "Do job 5",
+                        Deadline = DateTime.Now.AddDays(3),
+                        IsDone = true
+                    },
+                    new Todo
+                    {
+                        Content = "Do job 6",
+                        Deadline = DateTime.Now.AddMonths(1),
+                        IsDone = false
+                    }
+                });
+        }
         #endregion
     }
 }
